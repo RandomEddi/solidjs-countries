002699d6009114c52d42e0916a2ab782e43be781
ivan <ivansavichev884@gmail.com>
2023-07-01 13:54:01 +0300

    Add: tailwind, devtools, alias

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 /* @refresh reload */
 import { render } from 'solid-js/web'
-
+import { App } from './App'
 import './index.css'
-import App from './App'
+import 'solid-devtools'
 
 const root = document.getElementById('root')
 
@@ -12,8 +12,4 @@ if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
   )
 }
 
-render(
-  () => <App />,
-
-  root!,
-)
+render(() => <App />, root!)
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite'
 import solidPlugin from 'vite-plugin-solid'
-// import devtools from 'solid-devtools/vite';
+import devtools from 'solid-devtools/vite'
+import path from 'path'
 
 export default defineConfig({
   plugins: [
-    /* 
-    Uncomment the following line to enable solid-devtools.
-    For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
-    */
-    // devtools(),
+    devtools({
+      autoname: true,
+    }),
     solidPlugin(),
   ],
   server: {
@@ -16,5 +15,10 @@ export default defineConfig({
   },
   build: {
     target: 'esnext',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
   },
 })
