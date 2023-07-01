890bd16bcbe6d9b322895f75bb94253d3eae5494
ivan <ivansavichev884@gmail.com>
2023-07-01 13:57:01 +0300

    Fix: coutries styles

--- src/components/Country.tsx
+++ src/components/Country.tsx
@@ -3,8 +3,8 @@ import type { CountryInterface } from '@/types'
 
 export const Country: Component<CountryInterface> = (props) => {
   return (
-    <div class='flex gap-3 items-center hover:bg-white cursor-pointer'>
-      <img class='w-12' src={props.flags.png} alt={props.flags.alt} />
+    <div class='flex gap-3 items-center hover:bg-gray-300 cursor-pointer'>
+      <img class='w-12 h-8' src={props.flags.png} alt={props.flags.alt} />
       <p>{props.name.common}</p>
     </div>
   )
