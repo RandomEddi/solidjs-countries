6c25f6fb54651d0b1b4a20781d8f7152f6f26db5
ivan <ivansavichev884@gmail.com>
2023-07-03 18:57:36 +0300

    Add: Search input styles

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -10,6 +10,8 @@ export const Search: Component<Props> = ({ searchValue, setSearchValue }) => {
     <div class='flex justify-center pt-4'>
       <input
         type='text'
+        placeholder='Поиск...'
+        class='focus:outline-none px-2 rounded-lg text-2xl placeholder:text-xl'
         value={searchValue()}
         onInput={(event) => setSearchValue(event.target.value)}
       />
